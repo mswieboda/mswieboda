2109cfd77d33c7593a71d7b88aa3f74314185ed6
Matt Swieboda <mswiebod@gmail.com>
2014-04-15 16:08:36 -0500

    Added home#index, and bootstrap

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Mswieboda
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Add fonts (only Bootstrap Glyphicons currently)
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
   end
 end
